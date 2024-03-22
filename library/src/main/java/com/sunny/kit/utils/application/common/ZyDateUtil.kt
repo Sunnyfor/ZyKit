7841e5f4ed19d076c02743e92d8f064903622bc9
zhangye <zhangye98@foxmail.com>
2024-03-22 14:07:41 +0800

    补交时间工具类

--- library/src/main/java/com/sunny/kit/utils/application/common/ZyDateUtil.kt
+++ library/src/main/java/com/sunny/kit/utils/application/common/ZyDateUtil.kt
@@ -9,10 +9,17 @@ interface ZyDateUtil {
     /**
      * 根据格式获取当前时间
      */
-    fun getCurrentTime(pattern: String = "yyyy-MM-dd HH:mm:ss"):String
+    fun getCurrentTime(pattern: String = "yyyy-MM-dd HH:mm:ss"): String
 
     /**
      * 格式化时间
+     * return 格式化后的时间
      */
-    fun formatTime(date: Long,pattern: String ="yyyy-MM-dd HH:mm:ss"): String
+    fun formatTime(date: Long, pattern: String = "yyyy-MM-dd HH:mm:ss"): String
+
+    /**
+     * 格式化时间
+     * return 时间戳
+     */
+    fun formatTime(date: String, pattern: String = "yyyy-MM-dd HH:mm:ss"): Long
 }
